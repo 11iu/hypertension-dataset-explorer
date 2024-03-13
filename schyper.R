7f57684f9e3839b5d3398af775cbeab2d2ff2066
11iu <liu.louis91@gmail.com>
2024-03-12 17:16:53 -0700

    organize into datasets directory

--- schyper.R
+++ schyper.R
@@ -11,7 +11,7 @@ library(plotly)
 # Some initial setup:
 # this will not work if underscores are in the orig.ident (only for some views)
 # take in the file, get list of genes, get metadata numbers and categories, get pcs 1-9, and factors..
-aggregate <- readRDS('mouse.MCA.RNA.anno.v2.rds')
+aggregate <- readRDS('datasets/mouse.MCA.RNA.anno.v2.rds')
 genes = aggregate@assays$RNA
 reductions <- attributes(aggregate@reductions)
 meta_nums <- colnames(dplyr::select_if(aggregate@meta.data, is.numeric))
